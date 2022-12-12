60acf255b386d74f6b239631084efc57058c9fc2
FMinimal <fminimal@proton.me>
2022-12-12 05:11:12 +0100

    Add Pascal implementation

---
+++ truth-machine.pas
@@ -0,0 +1,13 @@
+program Truth_machine;
+var truth: integer;
+begin
+    readLn(input, truth);
+    while true do
+    begin
+        writeLn (output, truth);
+        if truth = 0 then
+        begin
+            break;
+        end;
+    end;
+end.
